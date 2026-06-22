5fef71e396376db5ac1c636e815e0d86c84279a4
anannyasunny79@gmail.com@gmail.com <anannyasunny79@gmail.com>
2026-06-22 13:47:13 +0530

    chore: clean up next.config.js tracing root configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,6 +9,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 module.exports = nextConfig;
